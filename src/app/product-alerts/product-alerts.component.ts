c9b0e5f098ece0d960348de4a860d36fe9b65b8c
jeo19 <terrakkk@outlook.com>
2021-10-27 09:19:38 +0800

    feat:Pass Event handler to parent component from child component

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../products';
-import { Input } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -8,8 +8,8 @@ import { Input } from '@angular/core';
   styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent implements OnInit {
-  @Input() product!: Product;
-
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
   constructor() {}
 
   ngOnInit() {}
