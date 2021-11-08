c8814a07ca470c36ddb2684934e05ba8efa9cfc6
jeo19 <terrakkk@outlook.com>
2021-11-08 23:48:47 +0800

    feat:Add routerLink property in top-bar templete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import {CartComponent} from './cart/cart.component'
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -17,6 +17,7 @@ import {CartComponent} from './cart/cart.component'
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
     ]),
   ],
   declarations: [
@@ -25,7 +26,7 @@ import {CartComponent} from './cart/cart.component'
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
   ],
   bootstrap: [AppComponent],
 })
